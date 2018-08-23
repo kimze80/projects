53e7219293886f0242f2d36f74ae1f04848a8375
kimze80 <kimze80@gmail.com>
2018-08-23 17:40:18 -0300

    display day time

---
+++ javascript/display_day_time/display.js
@@ -0,0 +1,5 @@
+const today = new Date();
+const day = today.getDay();
+const daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+
+console.log(`Today is : ${daylist[day]}.`);
